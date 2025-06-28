f3dcd3e4362efe556d86b90843e53eb43af971ae
CadeJordan <cade.m.jordan@gmail.com>
2025-06-27 21:52:15 -0700

    Minor on hover updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,10 @@ const Navigation = () => {
   return (
     <nav className="from-aisc-blue to-aisc-pink font-aisc-main bg-linear-to-r/shorter">
       <div className="flex justify-between py-4 text-white">
-        <Link href="/" className="ml-[2%] flex items-center gap-4">
+        <Link
+          href="/"
+          className="ml-[2%] flex items-center gap-4 hover:scale-105"
+        >
           <Image src={AISClogo} alt="logo" className="w-10" />
           <div className="text-2xl lg:text-3xl">UCR AISC</div>
         </Link>
@@ -33,7 +36,7 @@ const Navigation = () => {
             </div>
           ))}
           <Link
-            href="/"
+            href="https:/linktr.ee/aiscucr"
             className="duration 150 flex items-center rounded-full bg-white px-6 py-2 text-black hover:scale-105 md:px-6 lg:px-8"
           >
             <div className="text-2xl lg:text-3xl">Join</div>
