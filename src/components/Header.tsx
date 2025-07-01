3f8f0dcd34352fabad9c5470ef55e5b9ccdba347
Matthew Garcia <gmatthew2765@gmail.com>
2025-06-30 19:16:25 -0700

    inserted styles to parent div

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,9 +8,9 @@ type HeaderProps = {
 
 const Header = ({ text, image, alt }: HeaderProps) => {
   return (
-    <>
+    <div className="flex w-full flex-col items-center">
       <span className="from-aisc-blue to-aisc-pink block h-1 w-full bg-gradient-to-r" />
-      <div className="relative flex h-[10vh] flex-row items-center justify-center text-3xl md:h-[25vh] md:text-5xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
+      <div className="relative flex h-[10vh] min-w-screen flex-col items-center justify-center text-3xl md:h-[25vh] md:text-5xl lg:text-7xl xl:text-8xl 2xl:text-9xl">
         <Image
           className="w-screen object-cover opacity-75"
           fill
@@ -20,7 +20,7 @@ const Header = ({ text, image, alt }: HeaderProps) => {
         <p className="z-10 font-bold text-white">{text}</p>
       </div>
       <span className="from-aisc-blue to-aisc-pink block h-1 w-full bg-gradient-to-r" />
-    </>
+    </div>
   );
 };
 
