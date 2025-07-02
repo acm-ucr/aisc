2bd3c8823e71e6acb100e0e2d1a8305494fe2adc
Eros846 <karkisarik.2021@gmail.com>
2025-07-02 00:23:35 -0700

    Added changes to matchfigma.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,12 @@ import { footerLinks, Address } from "../data/FooterLinks";
 
 const Footer = () => {
   return (
-    <div className="font-aisc-main text-aisc-lightgray bg-aisc-black flex flex-row items-center justify-between p-4 text-2xl">
-      <div id="footer-information" className="flex flex-col">
+    <div className="font-aisc-main bg-aisc-black flex flex-row items-center justify-between px-14 pt-10 pb-18 text-white sm:text-xl md:text-2xl lg:text-3xl">
+      <div id="footer-information" className="flex flex-col space-y-3">
         <Link href={"/"}>UCR AI Student Collective</Link>
         <p>{Address.address}</p>
       </div>
-      <div className="flex flex-row space-x-5">
+      <div className="flex flex-row space-x-14">
         {footerLinks.map(({ link, icon }) => (
           <div key={link} className="flex items-center justify-center">
             <Link href={link}>{icon}</Link>
--- src/data/FooterLinks.tsx
+++ src/data/FooterLinks.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { SiInstagram, SiDiscord } from "react-icons/si";
-
 import { LiaLinkSolid } from "react-icons/lia";
-
 import { FaLinkedinIn } from "react-icons/fa";
-import { CiMail } from "react-icons/ci";
+import { IoIosMail } from "react-icons/io";
 
 interface socialMedia {
   link: string;
@@ -14,22 +12,25 @@ interface socialMedia {
 export const footerLinks: socialMedia[] = [
   {
     link: "https://www.linkedin.com/company/ai-student-collective/",
-    icon: <FaLinkedinIn />,
+    icon: <FaLinkedinIn className="h-10 w-10" />,
   },
   {
     link: "https://www.instagram.com/aisc_riv/",
-    icon: <SiInstagram />,
+    icon: <SiInstagram className="h-10 w-10" />,
   },
   {
     link: "https://discord.com/invite/z9ngFH4DVt",
-    icon: <SiDiscord />,
+    icon: <SiDiscord className="h-10 w-10" />,
   },
   {
     link: "mailto:aiscriverside@humansforai.com",
-    icon: <CiMail />,
+    icon: <IoIosMail className="h-10 w-10" />,
   },
 
-  { link: " https://humansforai.com/aisc/", icon: <LiaLinkSolid /> },
+  {
+    link: " https://humansforai.com/aisc/",
+    icon: <LiaLinkSolid className="h-10 w-10" />,
+  },
 ];
 
 export const Address = {
