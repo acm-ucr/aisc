b8fb6cdd05b4bfcccdc6309e8aa71f02d1f0faee
lbojo006 <luisbojorquezgoal@gmail.com>
2025-07-03 10:41:34 -0700

    Small Change from Initial Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Landing from "@/components/home/Landing";
 
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="flex items-center justify-center">
       <Landing />
     </div>
   );
