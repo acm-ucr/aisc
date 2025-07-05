0394fe0d61992bde47bfe52191168457dbe855a7
andersAsch0 <84699083+andersAsch0@users.noreply.github.com>
2025-07-05 15:38:09 -0600

    chatbot project description (placeholder image and alt)

--- src/components/projects/ChatbotDescription.tsx
+++ src/components/projects/ChatbotDescription.tsx
@@ -1,21 +1,23 @@
+import Image from "next/image";
+import chatbotImage from "@/public/aisc_logo.webp"; //replace with actual image
+
 const ChatbotDescription = () => {
   return (
-    <div className="flex w-full flex-row pt-5 text-[20px]">
-      <div className="flex w-1/3 bg-white"> </div>
-      <div className="m-3 flex w-2/3 flex-col gap-5 text-right text-pretty">
-        <div className="flex w-full bg-white font-bold">
-          <p> Personal Assistant Chatbot </p>
-        </div>
-        <div className="flex w-full bg-white">
-          <p>
-            {" "}
-            Make your own personal assisstant chatbot, designed to set
-            reminders, answer questions, and even provide current weather
-            updates! Gain Python skills in a practical scenario, get hands-on
-            experience with chatbot development and learn Natural Language
-            Processing Concepts!
-          </p>
-        </div>
+    <div className="flex w-full flex-col gap-4 pt-10 pr-10 pl-7 text-right text-[20px]/7 text-pretty">
+      <p className="font-bold"> Personal Assistant Chatbot </p>
+      <div className="flex w-full flex-row gap-10">
+        <Image
+          src={chatbotImage}
+          alt="Add Alt here" //replace with actual alt
+          className="h-50 w-1/3 rounded-3xl"
+          style={{ objectFit: "cover" }}
+        />
+        <p className="w-2/3">
+          Make your own personal assisstant chatbot, designed to set reminders,
+          answer questions, and even provide current weather updates! Gain
+          Python skills in a practical scenario, get hands-on experience with
+          chatbot development and learn Natural Languge Processing Concepts!
+        </p>
       </div>
     </div>
   );
