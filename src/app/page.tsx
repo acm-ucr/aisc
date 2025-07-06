39133abb2aa48e2b4f32b90ffed1e7bbe2a2bfd8
CadeJordan <cade.m.jordan@gmail.com>
2025-07-06 15:07:01 -0700

    Statistics Bar Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Landing from "@/components/home/Landing";
+import StatisticsBar from "@/components/home/StatisticsBar";
 
 const Home = () => {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex flex-col items-center justify-center">
       <Landing />
+      <StatisticsBar />
     </div>
   );
 };
