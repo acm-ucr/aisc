5df4ce5c3bbe1ac6903b6818dfbb14016bef4097
Bongo <kekoa5254@gmail.com>
2025-07-07 16:57:15 -0700

    Completed Car Price Predictor Description

---
+++ src/components/projects/CarPriceDescription.tsx
@@ -0,0 +1,15 @@
+const CarPriceDescription = () => {
+  return (
+    <div className="to-aisc-blue/50 bg-gradient-to-r from-white p-5">
+      <div className="flex w-2/3 flex-col gap-2 p-5">
+        <p className="text-xl font-bold">Car Price Predictor</p>
+        <p className="text-lg">
+          Join our journey into the exciting world of data science as we dive
+          into the topic of predictive modeling!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CarPriceDescription;
