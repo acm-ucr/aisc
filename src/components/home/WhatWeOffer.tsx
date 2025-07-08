fa230ae774ea898efde5d5b294fca4907fc3a776
Jeffrey <Jeffx2016@gmail.com>
2025-07-08 13:34:35 +0800

    finished WhatweOffer

---
+++ src/components/home/WhatWeOffer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const WhatWeOffer = () => {
+  return (
+    <div className="mx-auto my-20 flex w-1/2 flex-col items-center text-center">
+      <p className="w-1/2 text-2xl font-semibold">What we Offer</p>
+      <p className="my-6 text-xl leading-6 text-balance">
+        From hands-on technical workshops and speaker panels to career
+        development events and interdisciplinary projects, AISC Riverside offers
+        opportunities to grow in technical, creative, and entrepreneurial
+        directions. Whether you're just curious about AI or building your own
+        model from scratch, you'll find a supportive, inspiring community here.
+        Join us and help shape the future of AI at UCR!
+      </p>
+    </div>
+  );
+};
+
+export default WhatWeOffer;
