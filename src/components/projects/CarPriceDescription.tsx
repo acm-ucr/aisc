02e2e410bda7a9edbd7c947b8c66097958b1ae5e
Bongo <kekoa5254@gmail.com>
2025-07-08 15:37:32 -0700

    Added placeholder image

--- src/components/projects/CarPriceDescription.tsx
+++ src/components/projects/CarPriceDescription.tsx
@@ -1,6 +1,9 @@
+import Image from "next/image";
+import Logo from "@/public/aisc_logo.webp";
+
 const CarPriceDescription = () => {
   return (
-    <div className="to-aisc-blue/50 bg-gradient-to-r from-white p-5">
+    <div className="to-aisc-blue/50 flex flex-row bg-gradient-to-r from-white p-5">
       <div className="flex w-2/3 flex-col gap-2 p-5">
         <p className="text-xl font-bold">Car Price Predictor</p>
         <p className="text-lg">
@@ -8,6 +11,11 @@ const CarPriceDescription = () => {
           into the topic of predictive modeling!
         </p>
       </div>
+      <Image
+        className="mx-auto h-[20vh] w-[25vw] rounded-2xl"
+        src={Logo}
+        alt="Car Price Predictor Logo"
+      />
     </div>
   );
 };
