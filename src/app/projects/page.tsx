7950c83268b7e8e12ed9809a6e703578af823fbe
Naima Cano <naimacano@Naimas-MacBook-Pro.local>
2025-07-08 00:03:29 -0700

    Implementented SkinLesionsDescription component with image for now

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,13 +1,14 @@
 import ChatbotDescription from "@/components/projects/ChatbotDescription";
 import CarPriceDescription from "@/components/projects/CarPriceDescription";
 import HotelBookingApp from "@/components/projects/HotelBookingApp";
-
+import SkinLesionsDescription from "@/components/projects/SkinLesionsDescription";
 const Projects = () => {
   return (
     <>
       <CarPriceDescription />
       <HotelBookingApp />
       <ChatbotDescription />
+      <SkinLesionsDescription />
     </>
   );
 };
