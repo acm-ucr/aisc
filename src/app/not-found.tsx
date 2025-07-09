e1961e0337efcaac30fffa8e34ff5a0434d87120
Eros846 <karkisarik.2021@gmail.com>
2025-07-09 11:25:03 -0700

    Added 404 text and designs.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,22 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 const NotFound = () => {
-  return <div>404 Page Not Found</div>;
+  const router = useRouter();
+  const textStyle =
+    "[text-shadow:-1px_-1px_0_#FFF,1px_-1px_0_#FFF,-1px_1px_0_#FFF,1px_1px_0_#FFF,2px_4px_4.4px_#51B7FF]";
+
+  return (
+    <div className="from-aisc-pink to-aisc-blue flex min-h-screen min-w-screen items-center justify-center bg-gradient-to-r">
+      <div className="from-aisc-blue to-aisc-pink flex flex-col rounded-2xl bg-gradient-to-r p-[3px]">
+        <div className="bg-aisc-gray font-aisc-alt h-full w-full rounded-2xl px-80 py-30">
+          <p className={`${textStyle} text-9xl`}>404</p>
+          <p className={`${textStyle} text-6xl`}>ERROR</p>
+          <Button onClick={() => router.push("/")}>Home</Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
