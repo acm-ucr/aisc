2553066dfc2238b93f25fe4445e56e697e9a2f59
Neguyen-Edy <91765003+Neguyen-Edy@users.noreply.github.com>
2025-07-08 22:47:45 -0700

    More changes to the HotelBookingApp component

--- src/components/projects/HotelBookingApp.tsx
+++ src/components/projects/HotelBookingApp.tsx
@@ -5,15 +5,15 @@ const HotelBookingApp = () => {
   return (
     <div className="to-aisc-blue/50 flex flex-row items-start bg-gradient-to-r from-white p-3">
       <div className="flex w-2/3 flex-col gap-2 p-5">
-        <p className="text-xl font-bold">Hotel Booking App</p>
-        <p className="text-lg">
+        <p className="text-2xl font-bold">Hotel Booking App</p>
+        <p className="text-xl">
           This project involves designing a hotel booking app to cater to
           travelers needs. The user-friendly platorm will provide a seemless
           booking experience for users.
         </p>
       </div>
       <Image
-        className="mx-auto h-[20vh] w-[25vw] rounded-2xl"
+        className="mx-auto h-[20vh] w-[25vw] rounded-2xl object-cover"
         src={Logo}
         alt="Hotel Booking App Logo"
       />
