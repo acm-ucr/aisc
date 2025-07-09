8281c2c995513a23cde89b952119e099b62cef56
andersAsch0 <84699083+andersAsch0@users.noreply.github.com>
2025-07-08 21:06:27 -0700

    evened padding, simplified code, prevented image from becoming too large

--- src/components/projects/ChatbotDescription.tsx
+++ src/components/projects/ChatbotDescription.tsx
@@ -3,17 +3,16 @@ import chatbotImage from "@/public/aisc_logo.webp"; //replace with actual image
 
 const ChatbotDescription = () => {
   return (
-    <div className="flex w-full flex-col gap-4 pt-10 pr-10 pl-7 text-right text-[20px]/7 text-pretty">
-      <p className="font-bold"> Personal Assistant Chatbot </p>
-      <div className="flex w-full flex-row gap-10">
-        <Image
-          src={chatbotImage}
-          alt="Add Alt here" //replace with actual alt
-          className="h-50 w-1/3 rounded-3xl"
-          style={{ objectFit: "cover" }}
-        />
-        <p className="w-2/3">
-          Make your own personal assisstant chatbot, designed to set reminders,
+    <div className="flex w-full flex-row gap-10 p-10 text-right text-xl/7">
+      <Image
+        src={chatbotImage}
+        alt="Add Alt here" //replace with actual alt
+        className="w-[40vw] rounded-3xl object-cover md:h-50"
+      />
+      <div className="flex w-full flex-col gap-5">
+        <p className="font-bold"> Personal Assistant Chatbot </p>
+        <p>
+          Make your own personal assistant chatbot, designed to set reminders,
           answer questions, and even provide current weather updates! Gain
           Python skills in a practical scenario, get hands-on experience with
           chatbot development and learn Natural Languge Processing Concepts!
