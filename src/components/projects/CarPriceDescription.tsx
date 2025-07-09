c7944c6396035af90022f5f45b25cc5508c18cf2
Bongo <kekoa5254@gmail.com>
2025-07-08 20:45:35 -0700

    Minor changes

--- src/components/projects/CarPriceDescription.tsx
+++ src/components/projects/CarPriceDescription.tsx
@@ -5,14 +5,14 @@ const CarPriceDescription = () => {
   return (
     <div className="to-aisc-blue/50 flex flex-row bg-gradient-to-r from-white p-5">
       <div className="flex w-2/3 flex-col gap-2 p-5">
-        <p className="text-xl font-bold">Car Price Predictor</p>
-        <p className="text-lg">
+        <p className="text-2xl font-bold">Car Price Predictor</p>
+        <p className="text-xl">
           Join our journey into the exciting world of data science as we dive
           into the topic of predictive modeling!
         </p>
       </div>
       <Image
-        className="mx-auto h-[20vh] w-[25vw] rounded-2xl"
+        className="mx-auto h-[20vh] w-[25vw] rounded-2xl object-cover"
         src={Logo}
         alt="Car Price Predictor Logo"
       />
