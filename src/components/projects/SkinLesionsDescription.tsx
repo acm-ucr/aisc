f435778f1d40cfe88f2e5e40b2322db135062458
Naima Cano <naimacano@Naimas-MacBook-Pro.local>
2025-07-08 20:47:37 -0700

    resolved comments given during first pr

--- src/components/projects/SkinLesionsDescription.tsx
+++ src/components/projects/SkinLesionsDescription.tsx
@@ -6,18 +6,16 @@ const SkinLesionsDescription = () => {
     <div className="flex items-center bg-white p-3">
       <div className="flex w-1/3 justify-center">
         <Image
-          className="rounded-xl object-cover"
+          className="w-1/2 rounded-xl object-cover"
           src={Logo}
           alt="AISC Logo"
-          width={200}
-          height={100}
         />
       </div>
       <div className="w-2/3 px-4 text-right">
-        <p className="mb-0.5 flex-col text-xl font-bold text-black">
+        <p className="flex-col text-xl font-bold text-black">
           Skin Lesions Classification System
         </p>
-        <p className="text-l text-black">
+        <p className="text-lg text-black">
           Design a machine-learning model that utilizes computer vision to
           determine if a skin lesion is benign or malignant! Learn how to
           process medical data, check your model for biases, and utilize Python
