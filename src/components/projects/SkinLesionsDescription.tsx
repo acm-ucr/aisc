38aa78450cf7d123bac54641cb6f7a5bcde4d703
Naima Cano <naimacano@Naimas-MBP.lan>
2025-07-09 17:06:49 -0700

    had to run prettier again

--- src/components/projects/SkinLesionsDescription.tsx
+++ src/components/projects/SkinLesionsDescription.tsx
@@ -4,11 +4,11 @@ import Logo from "@/public/aisc_logo.webp";
 const SkinLesionsDescription = () => {
   return (
     <div className="flex items-center bg-white p-3">
-        <Image
-          className="flex w-1/3 rounded-xl object-cover"
-          src={Logo}
-          alt="AISC Logo"
-        />
+      <Image
+        className="flex w-1/3 rounded-xl object-cover"
+        src={Logo}
+        alt="AISC Logo"
+      />
       <div className="w-2/3 px-4 text-right">
         <p className="flex-col text-2xl font-bold text-black">
           Skin Lesions Classification System
