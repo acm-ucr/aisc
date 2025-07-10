326b81c662b63bbab43bc46d17e01329c3f04490
Eros846 <karkisarik.2021@gmail.com>
2025-07-09 17:19:13 -0700

    Fixed hydration issue.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,36 @@
 "use client";
 import { Button } from "@/components/ui/button";
 const NotFound = () => {
-  const textStyle =
-    "[text-shadow:-1px_-1px_0_#FFF,1px_-1px_0_#FFF,-1px_1px_0_#FFF,1px_1px_0_#FFF,2px_4px_4.4px_#51B7FF]";
-
   return (
     <div className="from-aisc-pink to-aisc-blue flex min-h-screen min-w-screen items-center justify-center bg-gradient-to-r">
       <div className="from-aisc-blue to-aisc-pink rounded-2xl bg-gradient-to-r p-[3px]">
         <div className="bg-aisc-gray flex h-full w-full flex-col items-center justify-center rounded-2xl px-110 py-15">
-          <p className={`${textStyle} font-aisc-alt text-[128px]`}>404</p>
-          <p className={`${textStyle} font-aisc-alt mb-10 text-6xl text-white`}>
+          <p
+            className={`font-aisc-alt text-[128px]`}
+            style={{
+              textShadow: `
+                -1px -1px 0 #FFF,
+                1px -1px 0 #FFF,
+                -1px 1px 0 #FFF,
+                1px 1px 0 #FFF,
+                2px 4px 4.4px #51B7FF
+        `,
+            }}
+          >
+            404
+          </p>
+          <p
+            className={`font-aisc-alt mb-10 text-6xl text-white`}
+            style={{
+              textShadow: `
+                -1px -1px 0 #FFF,
+                1px -1px 0 #FFF,
+                -1px 1px 0 #FFF,
+                1px 1px 0 #FFF,
+                2px 4px 4.4px #51B7FF
+              `,
+            }}
+          >
             ERROR
           </p>
           <Button
