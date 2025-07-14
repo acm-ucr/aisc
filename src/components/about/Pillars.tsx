d602020d32b04431509b8a834a5c9c7ab6f0fea9
andersAsch0 <84699083+andersAsch0@users.noreply.github.com>
2025-07-13 18:10:40 -0700

    vh instead of vw and x padding on image, destructured props, increased font size

--- src/components/about/Pillars.tsx
+++ src/components/about/Pillars.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import pillarsImage from "@/public/aisc_logo.webp"; //replace with actual image
+import pillarsImage from "@/public/placeholders/workshop.webp"; //replace with actual image
 
 const Pillars = () => {
   return (
     <div className="bg-aisc-gray flex w-full flex-col gap-10 px-20 py-10 text-center text-white">
-      <p className="text-2xl font-bold">Our 3 Pillars</p>
+      <p className="text-3xl font-bold">Our 3 Pillars</p>
       <div className="flex w-full flex-row place-content-between">
         <Pillar
           title="ACCESSIBILITY"
@@ -19,21 +19,23 @@ const Pillars = () => {
           description="Celebrating diversity in AI, fostering innovation through varied perspectives"
         />
       </div>
-      <Image
-        src={pillarsImage}
-        alt="Add Alt here" //replace with actual alt
-        className="h-[33vw] w-full rounded-4xl object-cover"
-      />
+      <div className = "w-full px-7"> 
+        <Image
+          src={pillarsImage}
+          alt="Add Alt here" //replace with actual alt
+          className="h-[33vh] w-full object-cover rounded-4xl"
+        />
+      </div>
     </div>
   );
 };
 export default Pillars;
 
-const Pillar = (props: { title: string; description: string }) => {
+const Pillar = ({ title, description}) => {
   return (
     <div className="flex w-[20vw] flex-col gap-5">
-      <p className="text-2xl font-bold">{props.title}</p>
-      <p className="text-xl leading-6">{props.description}</p>
+      <p className="text-3xl font-bold">{title}</p>
+      <p className="text-2xl leading-6">{description}</p>
     </div>
   );
 };
