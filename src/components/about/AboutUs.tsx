c1bd2bc8cdc14800ec44489975affe3de6cd8988
wesley <wesleylwu@gmail.com>
2025-07-16 11:35:17 -0700

    small padding change

--- src/components/about/AboutUs.tsx
+++ src/components/about/AboutUs.tsx
@@ -4,14 +4,14 @@ import aboutUsImageTwo from "@/public/placeholders/ambiguous.webp";
 
 const AboutUs = () => {
   return (
-    <div className="px-4 py-12">
+    <div className="p-12">
       <p className="mb-10 text-center text-3xl font-semibold">About Us</p>
       <div className="flex flex-col items-center justify-center gap-12 md:flex-row">
         <div className="flex max-w-md flex-col items-center text-center md:w-1/2">
           <Image
             src={aboutUsImageOne}
             alt="Our Values"
-            className="mb-4 h-[25vh] w-2/3 rounded-3xl object-cover"
+            className="mb-4 h-[25vh] w-[80vw] max-w-[300px] rounded-3xl object-cover"
           />
           <p className="mb-2 text-xl font-medium"> Our Values</p>
           <p>
@@ -23,9 +23,9 @@ const AboutUs = () => {
           <Image
             src={aboutUsImageTwo}
             alt="Our Values"
-            className="mb-4 h-[25vh] w-2/3 rounded-3xl object-cover"
+            className="mb-4 h-[25vh] w-[80vw] max-w-[300px] rounded-3xl object-cover"
           />
-          <p className="mb-2 text-xl font-medium"> Our Mission</p>
+          <p className="mb-2 text-xl font-medium"> Our Values</p>
           <p>
             Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
             ipsum Lorem ipsum
