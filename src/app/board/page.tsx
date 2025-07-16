1fe8565c6c7248dcb2dab124fd2cd0244dd2bf15
xwRongx <57518285+xwRongx@users.noreply.github.com>
2025-07-16 16:42:56 -0700

    fixed file type and requested changes

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,5 +1,18 @@
+import BoardCard from "@/components/board/Board";
+import Placeholder from "@/public/placeholders/headshot.png";
+
 const Board = () => {
-  return <div>Board</div>;
+  return (
+    <div>
+      <BoardCard
+        headshot={Placeholder}
+        name="Tanisha Jha"
+        role="President"
+        linkedin="www.google.com"
+      />
+      ;
+    </div>
+  );
 };
 
 export default Board;
--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,8 +1,15 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 import LinkedinLogo from "@/public/linkedin_logo.webp";
 
-const Board = ({ headshot, name, role, linkedin }) => {
+type Properties = {
+  headshot: StaticImageData;
+  name: string;
+  role: string;
+  linkedin: string;
+};
+
+const Board = ({ headshot, name, role, linkedin }: Properties) => {
   return (
     <div className="m-10 w-56 flex-col text-center">
       <Image
@@ -10,8 +17,8 @@ const Board = ({ headshot, name, role, linkedin }) => {
         alt="headshot"
         className="rounded-full object-center"
       />
-      <div className="font-aisc-main pt-4 text-2xl font-semibold">{name}</div>
-      <div className="font-aisc-main pt-4 text-2xl">{role}</div>
+      <div className="pt-4 text-2xl font-semibold">{name}</div>
+      <div className="pt-4 text-2xl">{role}</div>
       <div className="items-center justify-center pt-4">
         <Link href={linkedin}>
           <Image src={LinkedinLogo} alt="linkedin" className="mx-auto w-15" />
