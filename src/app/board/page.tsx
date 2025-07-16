c5fdd514c5d567fce9bb7276325f4ed732e5d052
xwRongx <57518285+xwRongx@users.noreply.github.com>
2025-07-16 16:52:03 -0700

    minor change

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,18 +1,5 @@
-import BoardCard from "@/components/board/Board";
-import Placeholder from "@/public/placeholders/headshot.png";
-
 const Board = () => {
-  return (
-    <div>
-      <BoardCard
-        headshot={Placeholder}
-        name="Tanisha Jha"
-        role="President"
-        linkedin="www.google.com"
-      />
-      ;
-    </div>
-  );
+  return <div>Board</div>;
 };
 
 export default Board;
