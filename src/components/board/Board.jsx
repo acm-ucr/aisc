76f6dd938d03c0037632472fbce2e99cd4c1a81d
xwRongx <57518285+xwRongx@users.noreply.github.com>
2025-07-15 22:55:29 -0700

    board card

---
+++ src/components/board/Board.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+import LinkedinLogo from "@/public/linkedin_logo.webp";
+
+const Board = ({ headshot, name, role, linkedin }) => {
+  return (
+    <div className="m-10 w-56 flex-col text-center">
+      <Image
+        src={headshot}
+        alt="headshot"
+        className="rounded-full object-center"
+      />
+      <div className="font-aisc-main pt-4 text-2xl font-semibold">{name}</div>
+      <div className="font-aisc-main pt-4 text-2xl">{role}</div>
+      <div className="items-center justify-center pt-4">
+        <Link href={linkedin}>
+          <Image src={LinkedinLogo} alt="linkedin" className="mx-auto w-15" />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Board;
