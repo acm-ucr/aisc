06e43173096bc31db3d40a3fafd3a14305ccfec8
qhgill <qhgill2@gmail.com>
2025-07-16 17:34:47 -0700

    small change

--- src/components/home/ProjectCard.tsx
+++ src/components/home/ProjectCard.tsx
@@ -10,13 +10,11 @@ interface ProjectCardProps {
 const ProjectCard: React.FC<ProjectCardProps> = ({ image, title, link }) => {
   return (
     <div className="mx-auto flex w-4/5 flex-col">
-      <div className="bg-aisc-gray/25 rounded-t-4xl">
-        <Image
-          src={image}
-          alt={title}
-          className="h-[30vh] w-full object-cover"
-        />
-      </div>
+      <Image
+        src={image}
+        alt={title}
+        className="bg-aisc-gray/25 h-[30vh] w-full rounded-t-4xl object-cover"
+      />
       <div className="bg-aisc-lightgray/25 flex flex-col gap-4 rounded-b-4xl p-8">
         <p className="text-4xl">{title}</p>
         <Link href={link} className="text-xl hover:underline">
