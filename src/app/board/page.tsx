a49c1ec3db5ddcf838882bd736410ba7247531cf
lbojo006 <luisbojorquezgoal@gmail.com>
2025-07-16 17:22:15 -0700

    Mobile Responsiveness

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,19 +1,20 @@
 const Board = () => {
   return (
-  <div>
-    <p>Board</p>
-    <div className="flex flex-col md:flex-row gap-4 m-6">
-      <div className="bg-red-300 flex-1 p-4 text-center rounded-lg">
-        Box 1
-      </div>
-      <div className="bg-green-300 flex-1 p-4 text-center rounded-lg">
-        Box 2
-      </div>
-      <div className="bg-yellow-300 flex-1 p-4 text-center rounded-lg">
-        Box 3
+    <div>
+      <p>Board</p>
+      <div className="m-6 flex flex-col gap-4 md:flex-row">
+        <div className="flex-1 rounded-lg bg-red-300 p-4 text-center">
+          Box 1
+        </div>
+        <div className="flex-1 rounded-lg bg-green-300 p-4 text-center">
+          Box 2
+        </div>
+        <div className="flex-1 rounded-lg bg-yellow-300 p-4 text-center">
+          Box 3
+        </div>
       </div>
     </div>
-  </div>);
+  );
 };
 
 export default Board;
