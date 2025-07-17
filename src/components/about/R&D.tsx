fb78e9c1bd7178acdffc9f7f0cd846b70ab02175
Jeffrey Xia <168297198+JeffreyXia05@users.noreply.github.com>
2025-07-17 02:06:27 -0700

    Update R&D.tsx

--- src/components/about/R&D.tsx
+++ src/components/about/R&D.tsx
@@ -6,19 +6,19 @@ import { Button } from "@/components/ui/button";
 const RandD = () => {
   return (
     <div className="mx-35 my-10 flex flex-col items-center justify-center">
-      <div className="text-2xl font-semibold">R&D</div>
+      <p className="text-2xl font-semibold">R&D</p>
       <div className="grid grid-cols-2 justify-center gap-10 pt-10">
         <Image
           src={Placeholder}
           alt="Image"
-          className="w-100 justify-self-center rounded-2xl"
+          className="w-3/5 justify-self-center rounded-2xl"
         />
         <div className="grid-row-2 grid">
-          <div className="pt-9 text-center text-xl">
+          <p className="pt-9 text-center text-xl">
             We write and publish articles related to AI research on medium's
             HumansForAI page with over 1K+ subscribers. Our team writes weekly
             newsletters with current job postings, AI industry updates and more!
-          </div>
+          </p>
           <Button
             asChild
             className="bg-aisc-blue h-13 w-45 justify-self-center rounded-4xl text-xl"
