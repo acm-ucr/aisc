ec0e70facf08fed59e6b8971696b9a97fe08bf76
Neguyen-Edy <91765003+Neguyen-Edy@users.noreply.github.com>
2025-07-19 09:26:15 -0700

    Implement Event Description component

---
+++ src/components/events/Description.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image"
+import Placeholder from "@/public/placeholders/ambiguous.webp";
+
+const Description = () => {
+  return (
+    <>
+        <div className="grid grid-cols-3 gap-20 p-10">
+            <div className="flex flex-col items-center gap-2">
+                <p className="font-bold text-lg self-start">
+                    Lorem Ipsum
+                </p>
+                <Image className="h-1/4 w-full rounded-4xl object-cover" src={Placeholder} alt="Placeholder Event" width={500} height={500}/>
+                <p className="text-center text-lg p-8">
+                    Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+                </p>
+            </div>
+            <div className="flex flex-col items-center gap-2">
+                <p className="font-bold text-lg self-start">
+                    Lorem Ipsum
+                </p>
+                <Image className="h-1/4 w-full rounded-4xl object-cover" src={Placeholder} alt="Placeholder Event" width={500} height={500}/>
+                <p className="text-center text-lg p-8">
+                    Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+                </p>
+            </div>
+            <div className="flex flex-col items-center gap-2">
+                <p className="font-bold text-lg self-start">
+                    Lorem Ipsum
+                </p>
+                <Image className="h-1/4 w-full rounded-4xl object-cover" src={Placeholder} alt="Placeholder Event" width={500} height={500}/>
+                <p className="text-center text-lg p-8">
+                    Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+                </p>
+            </div>
+            
+            
+        </div>
+    
+    </>
+  )
+}
+
+export default Description
