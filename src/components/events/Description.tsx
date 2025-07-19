648d84e3246aec1c7af3ba6ca5bc7f3c14d7774f
Neguyen-Edy <91765003+Neguyen-Edy@users.noreply.github.com>
2025-07-19 09:27:40 -0700

    Ran format on Event Description

--- src/components/events/Description.tsx
+++ src/components/events/Description.tsx
@@ -1,43 +1,55 @@
-import Image from "next/image"
+import Image from "next/image";
 import Placeholder from "@/public/placeholders/ambiguous.webp";
 
 const Description = () => {
   return (
     <>
-        <div className="grid grid-cols-3 gap-20 p-10">
-            <div className="flex flex-col items-center gap-2">
-                <p className="font-bold text-lg self-start">
-                    Lorem Ipsum
-                </p>
-                <Image className="h-1/4 w-full rounded-4xl object-cover" src={Placeholder} alt="Placeholder Event" width={500} height={500}/>
-                <p className="text-center text-lg p-8">
-                    Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                </p>
-            </div>
-            <div className="flex flex-col items-center gap-2">
-                <p className="font-bold text-lg self-start">
-                    Lorem Ipsum
-                </p>
-                <Image className="h-1/4 w-full rounded-4xl object-cover" src={Placeholder} alt="Placeholder Event" width={500} height={500}/>
-                <p className="text-center text-lg p-8">
-                    Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                </p>
-            </div>
-            <div className="flex flex-col items-center gap-2">
-                <p className="font-bold text-lg self-start">
-                    Lorem Ipsum
-                </p>
-                <Image className="h-1/4 w-full rounded-4xl object-cover" src={Placeholder} alt="Placeholder Event" width={500} height={500}/>
-                <p className="text-center text-lg p-8">
-                    Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
-                </p>
-            </div>
-            
-            
+      <div className="grid grid-cols-3 gap-20 p-10">
+        <div className="flex flex-col items-center gap-2">
+          <p className="self-start text-lg font-bold">Lorem Ipsum</p>
+          <Image
+            className="h-1/4 w-full rounded-4xl object-cover"
+            src={Placeholder}
+            alt="Placeholder Event"
+            width={500}
+            height={500}
+          />
+          <p className="p-8 text-center text-lg">
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+            ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+          </p>
         </div>
-    
+        <div className="flex flex-col items-center gap-2">
+          <p className="self-start text-lg font-bold">Lorem Ipsum</p>
+          <Image
+            className="h-1/4 w-full rounded-4xl object-cover"
+            src={Placeholder}
+            alt="Placeholder Event"
+            width={500}
+            height={500}
+          />
+          <p className="p-8 text-center text-lg">
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+            ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+          </p>
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <p className="self-start text-lg font-bold">Lorem Ipsum</p>
+          <Image
+            className="h-1/4 w-full rounded-4xl object-cover"
+            src={Placeholder}
+            alt="Placeholder Event"
+            width={500}
+            height={500}
+          />
+          <p className="p-8 text-center text-lg">
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+            ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+          </p>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Description
+export default Description;
