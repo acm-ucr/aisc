0ce3722f98188687c2dd4e70971b5d2818fe2221
Neguyen-Edy <91765003+Neguyen-Edy@users.noreply.github.com>
2025-07-21 00:47:26 -0700

    Add requested changes to Event Description component

--- src/components/events/Description.tsx
+++ src/components/events/Description.tsx
@@ -4,15 +4,15 @@ import Placeholder from "@/public/placeholders/ambiguous.webp";
 const Description = () => {
   return (
     <>
-      <div className="grid grid-cols-3 gap-20 p-10">
+      <div className="grid grid-cols-3 gap-18 p-10 my-5">
         <div className="flex flex-col items-center gap-2">
-          <p className="self-start text-lg font-bold">Lorem Ipsum</p>
+          <p className="self-start text-2xl font-bold">Lorem Ipsum</p>
           <Image
             className="h-1/4 w-full rounded-4xl object-cover"
             src={Placeholder}
             alt="Placeholder Event"
-            width={500}
-            height={500}
+            width={0}
+            height={0}
           />
           <p className="p-8 text-center text-lg">
             Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
@@ -20,13 +20,13 @@ const Description = () => {
           </p>
         </div>
         <div className="flex flex-col items-center gap-2">
-          <p className="self-start text-lg font-bold">Lorem Ipsum</p>
+          <p className="self-start text-2xl font-bold">Lorem Ipsum</p>
           <Image
             className="h-1/4 w-full rounded-4xl object-cover"
             src={Placeholder}
             alt="Placeholder Event"
-            width={500}
-            height={500}
+            width={0}
+            height={0}
           />
           <p className="p-8 text-center text-lg">
             Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
@@ -34,13 +34,13 @@ const Description = () => {
           </p>
         </div>
         <div className="flex flex-col items-center gap-2">
-          <p className="self-start text-lg font-bold">Lorem Ipsum</p>
+          <p className="self-start text-2xl font-bold">Lorem Ipsum</p>
           <Image
             className="h-1/4 w-full rounded-4xl object-cover"
             src={Placeholder}
             alt="Placeholder Event"
-            width={500}
-            height={500}
+            width={0}
+            height={0}
           />
           <p className="p-8 text-center text-lg">
             Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
