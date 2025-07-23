9111034bf804251e9d6a18783de56262cbe77d60
Matthew Garcia <gmatthew2765@gmail.com>
2025-07-22 21:34:22 -0700

    fixed carousel and added some y margins

--- src/components/home/Carousel.tsx
+++ src/components/home/Carousel.tsx
@@ -11,20 +11,22 @@ import {
 
 export default function CarouselDemo() {
   return (
-    <Carousel className="w-3/4 max-w-7xl">
-      <CarouselContent className="">
-        {Array.from({ length: 3 }).map((_, index) => (
-          <CarouselItem key={index}>
-            <div className="p-1">
-              <Card className="bg-aisc-lightgray">
-                <CardContent className="flex aspect-7/3 items-center justify-center"></CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+    <div className="my-20 flex w-full items-center justify-center">
+      <Carousel className="w-2/3 max-w-7xl md:w-3/4">
+        <CarouselContent className="">
+          {Array.from({ length: 3 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1">
+                <Card className="bg-aisc-lightgray">
+                  <CardContent className="flex aspect-7/3 items-center justify-center"></CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
   );
 }
