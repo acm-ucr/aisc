375ae4beac1bc440ad1a7af6d2082ed6271b30b9
CadeJordan <cade.m.jordan@gmail.com>
2025-07-29 14:52:54 -0700

    Mobile responsiveness for projects page

--- src/components/projects/RightAlignedProject.tsx
+++ src/components/projects/RightAlignedProject.tsx
@@ -10,13 +10,13 @@ interface ProjectsProps {
 
 const RightAlignedProject = ({ image, title, description }: ProjectsProps) => {
   return (
-    <div className="flex items-center bg-white p-3">
+    <div className="flex flex-col-reverse items-center bg-white p-3 md:flex-row">
       <Image
         className="mx-auto h-[20vh] w-[25vw] rounded-2xl object-cover"
         src={image}
         alt={title}
       />
-      <div className="flex w-2/3 flex-col gap-2 p-5 text-right">
+      <div className="flex w-2/3 gap-2 p-5 text-right">
         <p className="text-2xl font-bold">{title}</p>
         <p className="text-xl">{description}</p>
       </div>
