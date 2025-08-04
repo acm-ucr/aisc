dbb04682175903229f5dc0f25881f44ee2669b52
qhgill <qhgill2@gmail.com>
2025-08-04 13:18:12 -0700

    small fix

--- src/components/projects/LeftAlignedProject.tsx
+++ src/components/projects/LeftAlignedProject.tsx
@@ -10,7 +10,7 @@ interface ProjectsProps {
 
 const LeftAlignedProject = ({ image, title, description }: ProjectsProps) => {
   return (
-    <div className="to-aisc-blue/50 flex flex-col items-center bg-gradient-to-r from-white from-5% to-95% p-0 p-3 sm:flex-row">
+    <div className="to-aisc-blue/50 flex flex-col items-center bg-gradient-to-r from-white from-5% to-95% p-3 sm:flex-row">
       <div className="flex w-2/3 flex-col gap-2 py-5 md:p-5">
         <p className="text-lg font-bold md:text-2xl">{title}</p>
         <p className="text-sm md:text-xl">{description}</p>
