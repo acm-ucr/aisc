b4c8d5fc0b9225d491b0f8e7baf00bfae1f8d171
Neguyen-Edy <91765003+Neguyen-Edy@users.noreply.github.com>
2025-08-04 18:13:28 -0700

    Added mobile responsiveness to the Event description page

--- src/components/events/Description.tsx
+++ src/components/events/Description.tsx
@@ -4,8 +4,8 @@ import Placeholder from "@/public/placeholders/ambiguous.webp";
 const Description = () => {
   return (
     <>
-      <div className="my-5 grid grid-cols-3 gap-18 p-10">
-        <div className="flex flex-col items-center gap-2">
+      <div className="my-5 grid p-10 md:grid-cols-2 md:gap-10 xl:grid-cols-3 xl:gap-18">
+        <div className="flex flex-col items-stretch gap-2">
           <p className="self-start text-2xl font-bold">Lorem Ipsum</p>
           <Image
             className="h-1/4 w-full rounded-4xl object-cover"
