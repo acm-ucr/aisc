1bce2029da9303b3c4d32a00a055b88f5a02c385
Eros846 <karkisarik.2021@gmail.com>
2025-08-06 16:44:43 -0700

    Added mobile responsiveness to what we offer section.

--- src/components/home/WhatWeOffer.tsx
+++ src/components/home/WhatWeOffer.tsx
@@ -7,9 +7,9 @@ import speakerEventImage from "@/public/placeholders/speaker_event.webp";
 const WhatWeOffer = () => {
   return (
     <>
-      <div className="mx-auto mt-20 flex w-1/2 flex-col items-center text-center">
-        <p className="w-1/2 text-2xl font-semibold">What we Offer</p>
-        <p className="my-6 text-xl">
+      <div className="mx-auto mt-5 flex w-3/4 flex-col items-center text-center md:mt-20 md:w-1/2">
+        <p className="w-1/2 text-xl font-semibold md:text-3xl">What We Offer</p>
+        <p className="my-6 hidden text-xl md:block">
           From hands-on technical workshops and speaker panels to career
           development events and interdisciplinary projects, AISC Riverside
           offers opportunities to grow in technical, creative, and
@@ -19,43 +19,42 @@ const WhatWeOffer = () => {
           UCR!
         </p>
       </div>
-      <div className="mx-auto mt-5 mb-20 flex w-11/12 items-center justify-center space-x-10 text-center text-3xl">
-        <div className="flex flex-col">
+      <div className="mx-8 my-5 flex flex-wrap items-center justify-center gap-5 text-center text-sm md:mb-20 md:w-11/12 md:space-x-10 md:text-2xl lg:text-3xl">
+        <div className="flex flex-col items-center">
           <Image
             src={internEventImage}
             alt="Intern Event"
-            className="mb-5 h-[15vw] w-[17vw] overflow-hidden rounded-3xl object-cover"
+            className="mb-5 h-[35vw] w-[40vw] overflow-hidden rounded-3xl object-cover md:h-[15vw] md:w-[17vw]"
           />
 
           <p>NETWORKING</p>
           <p>EVENTS</p>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col items-center">
           <Image
             src={projectsImage}
             alt="Projects"
-            className="mb-5 h-[15vw] w-[17vw] overflow-hidden rounded-3xl object-cover"
+            className="mb-5 h-[35vw] w-[40vw] overflow-hidden rounded-3xl object-cover md:h-[15vw] md:w-[17vw]"
           />
 
           <p>TECHNICAL</p>
           <p>CONSULTING</p>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col items-center">
           <Image
             src={competitive}
             alt="Competitive Projects"
-            className="mb-5 h-[15vw] w-[17vw] overflow-hidden rounded-3xl object-cover"
+            className="mb-5 h-[35vw] w-[40vw] overflow-hidden rounded-3xl object-cover md:h-[15vw] md:w-[17vw]"
           />
 
           <p>CASE</p>
           <p>COMPETITIONS</p>
         </div>
-
-        <div className="flex flex-col">
+        <div className="flex flex-col items-center">
           <Image
             src={speakerEventImage}
             alt="Speaker Event"
-            className="mb-5 h-[15vw] w-[17vw] overflow-hidden rounded-3xl object-cover"
+            className="mb-5 h-[35vw] w-[40vw] overflow-hidden rounded-3xl object-cover md:h-[15vw] md:w-[17vw]"
           />
           <p>DYNAMIC</p>
           <p>WORKSHOPS</p>
