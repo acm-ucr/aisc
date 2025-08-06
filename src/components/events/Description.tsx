c5995c39bad497f4c42a267993388e7c8006fe88
Neguyen-Edy <91765003+Neguyen-Edy@users.noreply.github.com>
2025-08-05 19:59:16 -0700

    Made images smaller when viewed on mobile

--- src/components/events/Description.tsx
+++ src/components/events/Description.tsx
@@ -5,10 +5,10 @@ const Description = () => {
   return (
     <>
       <div className="my-5 flex flex-col p-10 md:flex-row md:gap-10 xl:gap-18">
-        <div className="flex flex-col items-center gap-2">
+        <div className="flex h-[50rem] flex-col items-center gap-2">
           <p className="self-start text-2xl font-bold">Lorem Ipsum</p>
           <Image
-            className="h-1/4 w-full rounded-4xl object-contain md:object-cover"
+            className="h-1/2 w-full rounded-4xl object-cover md:h-1/3"
             src={Placeholder}
             alt="Placeholder Event"
           />
@@ -17,10 +17,11 @@ const Description = () => {
             ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
           </p>
         </div>
-        <div className="flex flex-col items-center gap-2">
+
+        <div className="flex h-[50rem] flex-col items-center gap-2">
           <p className="self-start text-2xl font-bold">Lorem Ipsum</p>
           <Image
-            className="h-1/4 w-full rounded-4xl object-contain md:object-cover"
+            className="h-1/2 w-full rounded-4xl object-cover md:h-1/3"
             src={Placeholder}
             alt="Placeholder Event"
           />
@@ -29,10 +30,11 @@ const Description = () => {
             ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
           </p>
         </div>
-        <div className="flex flex-col items-center gap-2">
+
+        <div className="flex h-[50rem] flex-col items-center gap-2">
           <p className="self-start text-2xl font-bold">Lorem Ipsum</p>
           <Image
-            className="h-1/4 w-full rounded-4xl object-contain md:object-cover"
+            className="h-1/2 w-full rounded-4xl object-cover md:h-1/3"
             src={Placeholder}
             alt="Placeholder Event"
           />
