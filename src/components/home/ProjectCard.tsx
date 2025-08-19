e62f4fd21013f52f41a287e1b371224ed74f9ac0
Bongo <kekoa5254@gmail.com>
2025-08-19 15:28:59 -0700

    Animated project cards on What We Offer page

--- src/components/home/ProjectCard.tsx
+++ src/components/home/ProjectCard.tsx
@@ -12,22 +12,22 @@ interface ProjectCardProps {
 const ProjectCard: React.FC<ProjectCardProps> = ({ image, title, link }) => {
   return (
     <motion.div
-        className="mx-auto flex w-4/5 flex-col"
-        initial={{ opacity: 0, y: 20 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.75 }}
-      >
-    <Image
-      src={image}
-      alt={title}
-      className="bg-aisc-gray/25 h-[30vh] w-full rounded-t-4xl object-cover"
-    />
-    <div className="bg-aisc-lightgray/25 flex flex-col gap-4 rounded-b-4xl p-8">
-      <p className="text-4xl">{title}</p>
-      <Link href={link} className="text-xl hover:underline">
-        Learn More →
-      </Link>
-    </div>
+      className="mx-auto flex w-4/5 flex-col"
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.75 }}
+    >
+      <Image
+        src={image}
+        alt={title}
+        className="bg-aisc-gray/25 h-[30vh] w-full rounded-t-4xl object-cover"
+      />
+      <div className="bg-aisc-lightgray/25 flex flex-col gap-4 rounded-b-4xl p-8">
+        <p className="text-4xl">{title}</p>
+        <Link href={link} className="text-xl hover:underline">
+          Learn More →
+        </Link>
+      </div>
     </motion.div>
   );
 };
