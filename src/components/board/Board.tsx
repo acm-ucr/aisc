65207afdf98c486606c72e160c1084b573e0e7fb
Matthew Garcia <gmatthew2765@gmail.com>
2025-08-25 16:32:33 -0700

    enhanced board page with in view transitions

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -15,22 +15,29 @@ type Properties = {
 const Board = ({ headshot, name, role, linkedin }: Properties) => {
   return (
     <div className="s:w-105 mx-auto pb-14 text-center md:w-56">
-    <motion.div whileHover={{
-      scale: 1.05,
-      transition: { duration: 0.2 }
-    }}>
-      <Image
-        src={headshot}
-        alt="headshot"
-        className="aspect-square justify-center rounded-full object-cover"
-      />
-      <div className="pt-4 text-2xl font-semibold">{name}</div>
-      <div className="pt-4 text-2xl">{role}</div>
-      <div className="items-center justify-center pt-4">
-        <Link href={linkedin}>
-          <FaLinkedin className="fill-aisc-blue mx-auto h-15 w-15" />
-        </Link>
-      </div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{
+          opacity: 1,
+          transition: { duration: 0.5 },
+        }}
+        whileHover={{
+          scale: 1.05,
+          transition: { duration: 0.1 },
+        }}
+      >
+        <Image
+          src={headshot}
+          alt="headshot"
+          className="aspect-square justify-center rounded-full object-cover"
+        />
+        <div className="pt-4 text-2xl font-semibold">{name}</div>
+        <div className="pt-4 text-2xl">{role}</div>
+        <div className="items-center justify-center pt-4">
+          <Link href={linkedin}>
+            <FaLinkedin className="fill-aisc-blue mx-auto h-15 w-15" />
+          </Link>
+        </div>
       </motion.div>
     </div>
   );
